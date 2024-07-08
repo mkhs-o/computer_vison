a1f1ce4c94a62d5cc3edb6411552324c3638be5a
mkhs-o <mkhs-o@akane.waseda.jp>
2024-07-09 06:28:05 +0900

    [update] 時計の中心求めた

--- circle.py
+++ circle.py
@@ -1,34 +1,41 @@
+import cv2
 import numpy as np
-import cv2 as cv
+import matplotlib.pyplot as plt
 
-img_path = 'images/mackee.jpg'
- 
-img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
-assert img is not None, "file could not be read, check with os.path.exists()"
-img = cv.medianBlur(img,7)
-cimg = cv.cvtColor(img,cv.COLOR_GRAY2BGR)
- 
-circles = cv.HoughCircles(img,
-                          cv.HOUGH_GRADIENT,
-                          dp = 1,
-                          minDist = 100,
-                          param1=50,
-                          param2=60,
-                          minRadius=0,
-                          maxRadius=0)
-if not(len(circles)):
-    print("円検知不可")
-    exit()
+image_path = 'circles/IMG_9546.jpg'
+image = cv2.imread(image_path)
 
-circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
- # draw the outer circle
- cv.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
- # draw the center of the circle
- cv.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
- 
-cv.namedWindow('detected circles', cv.WINDOW_NORMAL)
-cv.resizeWindow('detected circles' ,800,600)
-cv.imshow('detected circles',cimg)
-cv.waitKey(0)
-cv.destroyAllWindows()
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+blurred = cv2.GaussianBlur(gray_image, (5, 5), 0)
+
+# ハフ変換を使用して円を検出する, dpは解像度の比率, minDistは円の中心  同士の最小距離
+circles = cv2.HoughCircles(blurred, 
+                           cv2.HOUGH_GRADIENT, 
+                           dp=1.5, 
+                           minDist=330,     #円の中心の最小距離、誤検出に関わる
+                           param1=100,      #Cannyエッジ検出の大きい方のしきい値
+                           param2=40,       #投票数の基準？
+                           minRadius=800,   #最小半径   
+                           maxRadius=1200   #最大半径
+                           )
+
+if circles is not None:
+    circles = np.round(circles[0, :]).astype("int")
+    x_sum = 0
+    y_sum = 0
+    for (x, y, r) in circles:
+
+        x_sum +=x
+        y_sum +=y
+        cv2.circle(image, (x, y), r, (0, 255, 0), 10)
+        cv2.circle(image, (x, y ), 30 , (0, 255, 0), 10)
+        # print(f"検出された円の中心: ({x}, {y}), 半径: {r}")
+    cv2.circle(image, (x_sum//len(circles), (y_sum//len(circles))), 30, (255, 0, 0), 10)
+else:
+    print("円が見つかりませんでした")
+# 結果を表示する
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+cv2.imwrite('hough_circles/7.png', image)
+plt.axis('off')
+plt.show()
