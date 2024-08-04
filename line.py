a38a42d723d6131833982e8dc98a9a6fe23894fb
mkhs-o <mkhs-o@akane.waseda.jp>
2024-08-05 00:40:50 +0900

    [update] 提出用にフォルダ整理を整理

--- line.py
+++ line.py
@@ -2,32 +2,35 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image_path = "circles/3.png"
+image_path = "clocks/7.png"
 image= cv2.imread(image_path)
 
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite('tmp.png', gray_image)
-blurred =cv2.GaussianBlur(gray_image, (5, 5), 0)
-edges = cv2.Canny(blurred, 50, 159, apertureSize = 3)
 
+blurred =cv2.GaussianBlur(gray_image, (5, 5), 0)
+
+edges = cv2.Canny(blurred, 20, 50, apertureSize = 3)    # Cannyエッジ検出
+cv2.imwrite('tmp.png', edges)
 #計算不可が少ない、長さを決められる, maxLineGap 検出された2本の線を1本とみなす間隔
 lines = cv2.HoughLinesP(edges,               
-                        rho = 1,                   # 極座標のρ
+                        rho = 1,              
                         theta = np.pi/100,
-                        threshold = 30,       # np.pi (radian)
-                        minLineLength = 1,  # 
-                        maxLineGap = 1  # 検出された2本の線を1本とみなす
+                        threshold = 100,       
+                        minLineLength = 15,    
+                        maxLineGap = 100       # 検出された2本の線を1本とみなす
                        )
-
+# 線分が見つかれば{x1, y1, x2, y2}のタプル、見つからなければNone
 if lines is not None:
     for line in lines:
+        # print(line[0])
         x1, y1, x2, y2 = line[0]
-        cv2.line(image, (x1, y1) ,(x2, y2), (0, 0, 255), 2)
+        cv2.line(image, (x1, y1) ,(x2, y2), (0, 0, 255), 10)
 else:
     print("検知なし")
 
 
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-cv2.imwrite('hough_lines/7.png', image)
+# cv2.imwrite('hough_lines/5.png', image)
+cv2.imwrite('0001.png',image)
 plt.axis('off')
 plt.show()
